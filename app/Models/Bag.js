f23be39a0e13cde2678ecdee98ef862bb00b9118
Marlon Secundo <marlon_secundo@outlook.com>
2019-04-23 10:54:00 -0300

    Bag Relationship Updated

--- app/Models/Bag.js
+++ app/Models/Bag.js
@@ -1,8 +1,10 @@
-
-
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
-class Bag extends Model {}
+class Bag extends Model {
+  user() {
+    this.belongsTo('App/Models/User');
+  }
+}
 
 module.exports = Bag;
